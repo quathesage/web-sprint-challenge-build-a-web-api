0c21393a6cfaf0b92a99d81cec3ecf2d7fd4d219
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-11 14:17:32 -0600

    added actions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,35 @@
-// add middlewares here related to actions
+const Action = require("./actions-model");
+
+async function checkActionId(req, res, next) {
+  try {
+    const action = await Action.get(req.params.id);
+    if (!action) {
+      res.status(404).json({
+        message: "Action id not found",
+      });
+    } else {
+      req.action = action;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Action not found",
+    });
+  }
+}
+
+function validateActionPost(req, res, next) {
+  const { project_id, descripton, notes } = req.body;
+  if (project_id && descripton && notes) {
+    next();
+  } else {
+    res.status(400).json({
+      message: "missing required post fields",
+    });
+  }
+}
+
+module.exports = {
+  checkActionId,
+  validateActionPost,
+};
